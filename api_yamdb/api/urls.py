395356ab7971e650e57ecc778aa3ddc6e6b52f7b
ingarbi <104501854+ingarbi@users.noreply.github.com>
2023-04-27 16:09:49 +0300

    user registration. Left 10 mistakes

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,9 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, UserSignupAPIView, TokenAPIView
+
+app_name = "api"
 
 router = DefaultRouter()
-
+router.register("users", UserViewSet, basename="users")
 
 urlpatterns = [
     path("v1/", include(router.urls)),
+    path("v1/auth/signup/", UserSignupAPIView.as_view(), name="signup"),
+    path("v1/auth/token/", TokenAPIView.as_view(), name="token_obtain"),
 ]
